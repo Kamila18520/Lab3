e9875405a844a1b98a7328576256fc05d395e4e5
Kamila18520 <104089237+Kamila18520@users.noreply.github.com>
2022-05-10 15:42:36 +0200

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,79 @@
+1. 
+git commit
+git commit
+2.
+git branch bugFix
+git checkout bugFix
+3.
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+4
+branch bugFix
+checkout bugFix
+commit
+checkout main
+commit
+checkout bugFix
+rebase main
+5
+checkout c4
+6.
+checkout bugFix
+checkout c4
+checkout HEAD~1
+7.checkout HEAD^
+branch -f main c6
+branch -fbugFix c0
+8.
+reset HEAD~1
+git cgeckout pushed
+git revert HEAD
+9. git cherry-pick c3 c4 c7 
+10 git rebase -i HEAD~4
+11.checkout main
+cherry-pick c4
+12.
+rebase -i main
+branch -f main c3'
+rebase -i main
+branch -f main c1
+rebase -i main
+branch -f main c3''
+13.
+checkout main
+cherry-pick c2
+branch -f main c1
+cherry-pick c2'
+cherry-pick c3
+14.
+checkout c1
+tag v0 c1
+chceckout c2
+tag v1 c2
+15
+commit
+16.
+checkout bugFix
+rebase -i main
+checkout side
+rebase -i bugFix
+checkout another
+rebase -i side
+branch -f main c7'
+17.
+branch bugWork
+branch -f bugWork c2
+18.
+git rebase -1 one
+branch -f main c5
+rebase -i onr
+branch -f main c2'
+rebase -i one c5'
+branch -f main c5
+branch -f three c2
+branch -f one c2'
+branch -f two c''
